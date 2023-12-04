8a087823f0501a3177acb5472e4d97a940f3feb0
iamback24x7 <107175697+iamback24x7@users.noreply.github.com>
2023-12-04 11:23:15 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,27 +9,27 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '27676335'))
-    API_HASH = str(getenv('API_HASH', '88e75ba7d40ffcbd6c51e35ad23fc7aa'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','6913343095:AAEiT_XHmhNjNljBEJT1JBSBRROtGjTghAI'))
+    API_ID = int(getenv('API_ID', '1222723'))
+    API_HASH = str(getenv('API_HASH', '67e6a613a39747035a30ad8eac363678'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN','6901144310:AAGIKfVppHbFLI0tiv_0F7HnTxMVSXZ9mTI'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002041089164'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002133996735'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '158.220.120.187'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1654566967").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "770118462").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'prathyoush'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'no_username_1bot'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN', 'fastdl.webappbazaar.com') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
